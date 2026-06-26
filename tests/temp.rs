15f5865fa2d9fd115b5a42db8d66274eaf495aa8
jeremy <jeremy.guillaume@centralesupelec.fr>
2026-06-26 14:38:49 +0200

    feat:integration test ok

--- tests/temp.rs
+++ tests/temp.rs
@@ -37,13 +37,59 @@ mod tests {
     #[test]
     #[cfg(target_arch = "aarch64")]
     fn test_aarch64() {
-        //TODO
-        assert!(false);
+       compute_mosaic(Options {
+       image: String::from("assets/kit.jpeg"),
+       output: String::from("outputCi.png"),
+       tiles: String::from("assets/tiles-small/images"),
+       scaling: 1,
+       tile_size: 25,
+       remove_used: false,
+       verbose: true,
+       simd: false,
+       num_thread: 1,
+   });
+   let img = ImageReader::open("outputCi.png")
+        .unwrap()
+        .decode()
+        .unwrap()
+        .into_rgb8();
+    let hashOutput = hash_image(&img);
+
+    let imggt = ImageReader::open("assets/ground-truth-kit.png")
+        .unwrap()
+        .decode()
+        .unwrap()
+        .into_rgb8();
+    let hashGroundTruth = hash_image(&img);
+    assert_eq!(hashOutput, hashGroundTruth);
     }
 
     #[test]
     fn test_generic() {
-        //TODO
-        assert!(false);
+        compute_mosaic(Options {
+       image: String::from("assets/kit.jpeg"),
+       output: String::from("outputCi.png"),
+       tiles: String::from("assets/tiles-small/images"),
+       scaling: 1,
+       tile_size: 25,
+       remove_used: false,
+       verbose: true,
+       simd: false,
+       num_thread: 1,
+   });
+   let img = ImageReader::open("outputCi.png")
+        .unwrap()
+        .decode()
+        .unwrap()
+        .into_rgb8();
+    let hashOutput = hash_image(&img);
+
+    let imggt = ImageReader::open("assets/ground-truth-kit.png")
+        .unwrap()
+        .decode()
+        .unwrap()
+        .into_rgb8();
+    let hashGroundTruth = hash_image(&img);
+    assert_eq!(hashOutput, hashGroundTruth);
     }
 }
